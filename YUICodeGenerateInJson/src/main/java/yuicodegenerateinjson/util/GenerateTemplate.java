c87e476cc01134d5d812b6d646bf3267716d8bf6
ashishprorigo <ashish.wagh@prorigosoftware.com>
2024-02-12 13:55:09 +0530

    first commit

---
+++ YUICodeGenerateInJson/src/main/java/yuicodegenerateinjson/util/GenerateTemplate.java
@@ -0,0 +1,15 @@
+package yuicodegenerateinjson.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class  GenerateTemplate {
+
+  private static void generateYuiTemplate(JsonNode jsonNode, StringBuilder yuiTemplateCode) {
+
+
+
+
+  }
+
+
+}
